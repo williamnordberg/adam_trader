ec579d23da7634027937d2f3158eb220ace2ee37
Will7i7am7 <william.nordberg3@gmail.com>
2023-02-01 17:17:08 +0800

    first commit

---
+++ run_spider.py
@@ -0,0 +1,13 @@
+from scrapy.crawler import CrawlerProcess
+from spiders.spider import BitcoinRichListSpider
+import pandas as pd
+import requests
+
+if __name__ == "__main__":
+    spider = BitcoinRichListSpider()
+    process = CrawlerProcess({
+        'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+    })
+    process.crawl(BitcoinRichListSpider)
+    process.start()
+
