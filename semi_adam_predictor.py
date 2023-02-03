1c0c05715276ab531f91464753e6dd21fdf04e02
Will7i7am7 <william.nordberg3@gmail.com>
2023-02-03 13:08:59 +0800

    first commit

--- semi_adam_predictor.py
+++ semi_adam_predictor.py
@@ -1,17 +1,18 @@
-import pandas as pd
+import lightgbm as lgb
 import numpy as np
+import pandas as pd
+from numpy import ravel
+from sklearn import preprocessing
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
-from sklearn import preprocessing
-from sklearn.metrics import r2_score
 from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import PolynomialFeatures, StandardScaler
+from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
 from sklearn.svm import SVR
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import RandomForestRegressor
-from numpy import ravel
-
+from xgboost import XGBRegressor
 
 data = pd.read_csv('C:/will/Python_project/adam_trader/datasets/BTC-to-merg.csv',
                    index_col='Date',
@@ -64,7 +65,7 @@ data_git = pd.read_csv('C:/will/Python_project/adam_trader/datasets/github.csv',
                        )
 data['Git'] = data_git['Open Issues']
 
-# Telegram has not enugh data
+# Telegram has not enough data
 # derivative_perpetual
 data_derivative_per = pd.read_csv('C:/will/Python_project/adam_trader/datasets/derivative_perpetual.csv',
                                   index_col='DateTime',
@@ -107,7 +108,7 @@ data22 = process_data(data, '2022', None, 'corr22.csv')
 # A1. calculate Chi_squerd on yearly dataset
 
 
-def feature_select(data_feature_select, year):
+def feature_select(data_feature_select, year_in_function):
     data_feature_select.dropna(axis=1, how='all')
     X = data_feature_select.iloc[:, 1:169]  # independent variable columns
     y = data_feature_select['Close']  # target variable column (price range)
@@ -123,7 +124,7 @@ def feature_select(data_feature_select, year):
     # concat two dataframes
     feature_scores = pd.concat([dfcolumns, dfscores], axis=1)
     feature_scores.columns = ['Specs', 'Score']  # naming the dataframe columns
-    print(f"Year {year}:")
+    # print(f"Year {year}:")
     # print(feature_scores.nlargest(10, 'Score'))   # printing 10 best features
 
 
@@ -138,11 +139,12 @@ feature_select(data22, 22)
 
 # A2. Regressions on yearly
 
-def mape(Y_Predicted, Y_actual):
-    mape = np.mean(np.abs((Y_actual - Y_Predicted)/Y_actual), axis=0)*100
-    return float(mape)
+def mean_absolute_percentage_error(y_true, y_pred):
+    y_true, y_pred = np.array(y_true), np.array(y_pred)
+    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
 
-def five_regression(data_to_regression):
+
+def seven_regression(data_to_regression):
     X = data_to_regression[['DiffLast', 'DiffMean', 'CapAct1yrUSD', 'HashRate', 'Open']]
     y = data_to_regression[['Close']]
     X_train, X_test, y_train, y_test = train_test_split(X, y,
@@ -153,7 +155,7 @@ def five_regression(data_to_regression):
     linear_model = LinearRegression()
     linear_model.fit(X_train, y_train)
     prediction = linear_model.predict(X_test)
-    print('Linear_mape:', mape(prediction, y_test))
+    print('MAPE_linear:', mean_absolute_percentage_error(prediction, y_test))
 
     # 2.polynomial
     polynomial = PolynomialFeatures(degree=3)
@@ -161,7 +163,7 @@ def five_regression(data_to_regression):
     model = LinearRegression()
     model.fit(X_polynomial, y_train)
     predictions_poly = model.predict(polynomial.transform(X_test))
-    print('MAPE_polynomial:', mape(predictions_poly, y_test))
+    print('MAPE_polynomial:', mean_absolute_percentage_error(predictions_poly, y_test))
 
     # 3.data scaling preprocessing +SVR Model
     X_scaler = StandardScaler()
@@ -169,17 +171,60 @@ def five_regression(data_to_regression):
     X_train_scaled = X_scaler.fit_transform(X_train)
     y_train_scaled = y_scaler.fit_transform(y_train)
     svr_model = SVR(kernel='rbf')
-    svr_model.fit(X_train_scaled, y_train_scaled)
+    svr_model.fit(X_train_scaled, ravel(y_train_scaled))
     X_test_scaled = X_scaler.transform(X_test)
     results = svr_model.predict(X_test_scaled).reshape(-1, 1)
     predictions_svr = y_scaler.inverse_transform(results)
-    print('\n MAPE_svr:', mape(predictions_svr, y_test))
+    print('MAPE_svr:', mean_absolute_percentage_error(predictions_svr, y_test))
 
     # 4.regression tree
     decision_model = DecisionTreeRegressor(random_state=0)
     decision_model.fit(X_train, y_train)
     predictions_tree = decision_model.predict(X_test).reshape(-1, 1)
-    print('\n MAPE_decision_tree:', mape(y_test, predictions_tree), )
+    print('MAPE_decision_tree:', mean_absolute_percentage_error(y_test, predictions_tree))
+
+    # 5.random forest
+    forest_model = RandomForestRegressor(n_estimators=3, random_state=0)
+    forest_model.fit(X_train, ravel(y_train))
+    predictions_forest = forest_model.predict(X_train)
+    # print('\n''r2_forest:',r2_score(y_train, predictions_forest))
+    print('MAPE_forest:', mean_absolute_percentage_error(y_test, predictions_forest))
+
+    # 6.ADA booster
+    import math
+    training_amount = 0.8
+    ROWS = data.shape[0]
+    index = math.floor(ROWS * training_amount)
+    train_x = data[['DiffLast', 'DiffMean', 'CapAct1yrUSD', 'HashRate', 'Open']][:index].to_numpy()
+    train_y = data['Close'][:index]
+
+    test_x = data[['DiffLast', 'DiffMean', 'CapAct1yrUSD', 'HashRate', 'Open']][index:].to_numpy()
+    test_y = data['Close'][index:]
+    from sklearn.ensemble import AdaBoostRegressor
+    model = AdaBoostRegressor()
+    model.fit(train_x, train_y)
+    predictions_ADA = model.predict(test_x)
+    pd.Series(predictions_ADA, index=test_y.index)
+    r2_score(test_y, predictions_ADA)
+    print('MAPE_ADA:', mean_absolute_percentage_error(test_y, predictions_ADA))
+
+    # 7. XGBoost
+    xgb_model = XGBRegressor(n_estimators=100, learning_rate=0.05, random_state=0)
+    xgb_model.fit(X_train, y_train)
+    xgb_predictions = xgb_model.predict(X_test)
+    xgb_mape = mean_absolute_percentage_error(y_test, xgb_predictions)
+    print("MAPE for XGBoost:", xgb_mape)
+
+    # 8. LightGBM
+    lgb_model = lgb.LGBMRegressor(boosting_type='gbdt', n_estimators=100, learning_rate=0.05, random_state=0)
+    lgb_model.fit(X_train, y_train)
+    lgb_predictions = lgb_model.predict(X_test)
+    lgb_mape = mean_absolute_percentage_error(y_test, lgb_predictions)
+    print("MAPE for LightGBM:", lgb_mape)
 
 
-five_regression(data22)
+years = [2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023]
+for year, data in zip(years, [data, data16, data17, data18, data19, data20, data21, data22]):
+    seven_regression(data)
+    print('\n', '***************', year, '***************', '\n')
+
