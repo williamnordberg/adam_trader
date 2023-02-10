eb4dd3c15dc67c1e3d277622a8ac2c6c067b3669
Will7i7am7 <william.nordberg3@gmail.com>
2023-02-10 11:59:19 +0800

    first commit

--- scrap_rate_CPI_PPI.py
+++ scrap_rate_CPI_PPI.py
@@ -1,69 +1,49 @@
-import fredapi
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 
+# there is a line that you can change to  what you want to scrap
+url = "https://www.forexfactory.com/calendar"
 
-url = "https://www.forexfactory.com/calendar?week=jan29.2023#closed"
-
-# Initialize a headless browser and set the user agent
 options = webdriver.ChromeOptions()
 options.add_argument("--headless")
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36")
 
-# Create the Chrome service
 service = Service(executable_path='C:\will\chromedriver.exe')
-
-# Start the Chrome service
 service.start()
 
-# Connect to the service and initialize a browser
 browser = webdriver.Remote(service.service_url, options=options)
-
-# Load the webpage
 browser.get(url)
-
-# Wait for the page to load
 browser.implicitly_wait(10)
 
-# Extract the page source
 html = browser.page_source
-
-# Create BeautifulSoup object from the source
 soup = BeautifulSoup(html, "html.parser")
-#print(soup)
 
 table = soup.find("table", class_="calendar__table")
 rows = table.find_all("tr")
-forecast_value = None
+forecast_value, forecast_value1, forecast_value1 = None, None, None
 for r in rows:
     event_cell = r.find("td", class_="calendar__cell calendar__event event")
     # just change here to what you want to search for
-    if event_cell and event_cell.find("span", class_="calendar__event-title").text == "Federal Funds Rate":
+    if event_cell and event_cell.find("span", class_="calendar__event-title").text == "Unemployment Claims":
         interest_cell = event_cell.find_next_sibling("td", class_="calendar__cell calendar__forecast forecast")
         forecast_value = interest_cell.string
+    if event_cell and event_cell.find("span", class_="calendar__event-title").text == "Construction PMI":
+        interest_cell = event_cell.find_next_sibling("td", class_="calendar__cell calendar__forecast forecast")
+        forecast_value1 = interest_cell.string
+    if event_cell and event_cell.find("span", class_="calendar__event-title").text == "Retail Sales m/m":
+        interest_cell = event_cell.find_next_sibling("td", class_="calendar__cell calendar__forecast forecast")
+        forecast_value2 = interest_cell.string
+    if forecast_value and forecast_value1 and forecast_value2:
         break
 
-if forecast_value:
-    print("Next expected forecast value:", forecast_value)
+if forecast_value and forecast_value1 and forecast_value2:
+    print("Next expected forecast value:", forecast_value, forecast_value1, forecast_value2)
 else:
     print("Forecast value not found")
 
 browser.quit()
 service.stop()
 
-
-# Getting current rate
-# Initialize the API
-fred = fredapi.Fred(api_key='8f7cbcbc1210c7efa87ee9484e159c21')
-
-# Get the latest value for the federal funds rate
-fed_funds_rate = fred.get_series("FEDFUNDS")
-
-# Get the latest value (i.e., the most recent observation)
-latest_value = fed_funds_rate.iloc[-1]
-
-# Print the result
-print("The current federal funds rate is {:.2f}%".format(latest_value))
 
 
