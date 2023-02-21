4772c4b6069974dc7fe72ecceec41ffa246c499f
Will7i7am7 <william.nordberg3@gmail.com>
2023-02-21 08:32:12 +0800

    first commit

--- main_decision.py
+++ main_decision.py
@@ -1,29 +1,27 @@
 from time import sleep
-
+from order_book import get_probabilities
 loop_counter = 0
+SYMBOLS = ['BTCUSDT', 'BTCBUSD']
 
+# trading opportunity
 while True:
     loop_counter += 1
     print(loop_counter)
-
-    # region 1. Get Adam Predictor value
-
-
     # region 1.1 Get the prediction
+
     # endregion
 
     # region 1.2 add new value to dataset
-
     # endregion
-
-    # endregion 1. Get Adam Predictor value
 
     # region 2. Get Adam Watcher value
     # endregion 2. Get Adam Watcher value
 
     # region 2.1 get order book
     # get probability of price go up and down
-    #
+    probability_down, probability_up = get_probabilities(SYMBOLS, bid_multiplier=0.995, ask_multiplier=1.005)
+    print('prob down and up', probability_down, probability_up)
+
     # endregion
 
     # region 2.2. Richest address on blockchain
@@ -52,8 +50,10 @@ while True:
     # region 2.9 Youtube
     # endregion
 
-
     # region 3.Make decision about the trade
     # endregion
 
     sleep(30)
+
+# region when a trade is open
+# endregion
