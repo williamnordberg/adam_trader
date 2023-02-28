5c8703c37c76c04f4a2cb5f5abc4c1a3762b37e3
Will7i7am7 <william.nordberg3@gmail.com>
2023-02-28 10:14:36 +0800

    first commit

--- google_search.py
+++ google_search.py
@@ -1,23 +1,72 @@
+from typing import List
+from pytrends.exceptions import ResponseError
 from pytrends.request import TrendReq
 import numpy as np
-pytrends = TrendReq(hl='en-US', tz=360)
+import requests
 
-kw_list = ["Bitcoin"]
 
-pytrends.build_payload(kw_list, cat=0, timeframe='now 7-d', geo='', gprop='')
+def check_internet_connection() -> bool:
+    """
+    Check if there is an internet connection.
 
-trend = pytrends.interest_over_time()
+    Returns:
+        bool: True if there is an internet connection, False otherwise.
+    """
+    try:
+        requests.get("http://www.google.com", timeout=3)
+        return True
+    except requests.ConnectionError:
+        print("No internet connection available.")
+        return False
 
-# Calculate the average increase in search volume
-search_volume = trend['Bitcoin'].values
-increase = np.diff(search_volume)
-#print(increase)
-average_increase = np.mean(increase)
-#print(average_increase)
 
-# Check if the last hour's search volume is significantly higher
-last_hour = trend.iloc[-1]['Bitcoin']
-if last_hour > ((search_volume[-2] + average_increase)*1.2):
-    print("There is an unusual increase in search in the last hour")
+def check_search_trend(keywords: List[str], threshold: float = 1.2) -> bool:
+    """
+    Check if there is a significant increase in search volume for a list of keywords in the past 7 days.
+
+    Args:
+        keywords (List[str]): The list of keywords to search for.
+        threshold (float): The threshold to use for determining if the last hour's search volume is significantly
+        higher. Default is 1.2, which means if the last hour's search volume is 20% higher than the expected value,
+        it is considered significant.
+
+    Returns:
+        bool: True if there is a significant increase in search volume for all keywords, False otherwise.
+    """
+
+    # Check for internet connection
+    if not check_internet_connection():
+        return False
+
+    # Convert all keywords to lowercase
+    keywords = [keyword.lower() for keyword in keywords]
+
+    try:
+        pytrends = TrendReq(hl='en-US', tz=360)
+
+        pytrends.build_payload(keywords, cat=0, timeframe='now 7-d', geo='', gprop='')
+
+        trend = pytrends.interest_over_time()
+
+        # Calculate the average increase in search volume for all keywords
+        search_volume = trend.values
+        increase = np.diff(search_volume, axis=0)
+        average_increase = np.mean(increase, axis=0)
+
+        # Check if the last hour's search volume is significantly higher for all keywords
+        last_hour = trend.iloc[-1].values
+        if any(last_hour <= ((search_volume[-2] + average_increase) * threshold)):
+            return False
+
+        return True
+
+    except ResponseError as e:
+        print(f"An error occurred: {e}")
+        return False
+
+
+result = check_search_trend(["Bitcoin", "ethereum"], threshold=1.2)
+if result:
+    print("There is a significant increase in search volume for all keywords.")
 else:
-    print("The search trend in the last hour is normal")
+    print("There is no significant increase in search volume for all keywords.")
--- test.py
+++ test.py
@@ -1,114 +1,72 @@
-from datetime import datetime, timedelta
+from typing import List
+from pytrends.exceptions import ResponseError
+from pytrends.request import TrendReq
+import numpy as np
 import requests
-import time
-import csv
-import pandas as pd
-from scrapy.crawler import CrawlerProcess
-from spider import BitcoinRichListSpider
 
 
-def read_addresses_from_csv(file_path):
-    addresses = []
-    with open(file_path, 'r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            addresses.append(row[0])
-    return addresses
+def check_internet_connection() -> bool:
+    """
+    Check if there is an internet connection.
+
+    Returns:
+        bool: True if there is an internet connection, False otherwise.
+    """
+    try:
+        requests.get("http://www.google.com", timeout=3)
+        return True
+    except requests.ConnectionError:
+        print("No internet connection available.")
+        return False
 
 
-def check_address_transactions(address):
-    # Get the current time and time 24 hours ago
-    current_time = int(time.time())
-    time_24_hours_ago = current_time - 864000
+def check_search_trend(keywords: List[str], threshold: float = 1.2) -> bool:
+    """
+    Check if there is a significant increase in search volume for a list of keywords in the past 7 days.
 
-    # Construct the API URL to get all transactions for the address
-    api_url = f"https://blockchain.info/rawaddr/{address}"
+    Args:
+        keywords (List[str]): The list of keywords to search for.
+        threshold (float): The threshold to use for determining if the last hour's search volume is significantly
+        higher. Default is 1.2, which means if the last hour's search volume is 20% higher than the expected value,
+        it is considered significant.
 
-    # Send the API request and get the response
-    response = requests.get(api_url)
+    Returns:
+        bool: True if there is a significant increase in search volume for all keywords, False otherwise.
+    """
 
-    # Check the response status code
-    if response.status_code == 200:
-        response_json = response.json()
+    # Check for internet connection
+    if not check_internet_connection():
+        return False
 
-        # Get the list of transactions for the address in the last 24 hours
-        transactions = [tx for tx in response_json["txs"] if tx["time"] > time_24_hours_ago]
+    # Convert all keywords to lowercase
+    keywords = [keyword.lower() for keyword in keywords]
 
-        # Initialize variables for tracking total sent and received
-        total_received = 0
-        total_sent = 0
+    try:
+        pytrends = TrendReq(hl='en-US', tz=360)
 
-        # Loop through the list of transactions and calculate total sent and received
-        for tx in transactions:
-            for input in tx["inputs"]:
-                if "prev_out" in input and input["prev_out"]["addr"] == address:
-                    total_sent += input["prev_out"]["value"]
-                else:
-                    for output in tx["out"]:
-                        if output["addr"] == address:
-                            total_received += output["value"]
+        pytrends.build_payload(keywords, cat=0, timeframe='now 7-d', geo='', gprop='')
 
-        # Convert from Satoshi to BTC
-        return total_received / 100000000, total_sent / -100000000
+        trend = pytrends.interest_over_time()
 
-    elif response.status_code == 429:
-        print(f"Rate limited for address {address}. Sleeping for a minute.")
-        time.sleep(60)
-        return check_address_transactions(address)
-    else:
-        print(f"Failed to get transaction history for address {address}. Status code: {response.status_code}")
-        return 0, 0
+        # Calculate the average increase in search volume for all keywords
+        search_volume = trend.values
+        increase = np.diff(search_volume, axis=0)
+        average_increase = np.mean(increase, axis=0)
 
+        # Check if the last hour's search volume is significantly higher for all keywords
+        last_hour = trend.iloc[-1].values
+        if any(last_hour <= ((search_volume[-2] + average_increase) * threshold)):
+            return False
 
-def check_multiple_addresses(addresses):
-    total_received = 0
-    total_sent = 0
+        return True
 
-    for address in addresses:
-        print(f"Checking transaction history for address {address}")
-        received, sent = check_address_transactions(address)
-        total_received += received
-        total_sent += sent
-        print(f"Total Bitcoin received in the last 24 hours: {total_received} BTC")
-        print(f"Total Bitcoin sent in the last 24 hours: {total_sent} BTC")
-        time.sleep(6)
-
-    print(f"Total Bitcoin received in the last 24 hours: {total_received / 100000000} BTC")
-    print(f"Total Bitcoin sent in the last 24 hours: {total_sent / 100000000} BTC")
-
-    return total_received, total_sent
+    except ResponseError as e:
+        print(f"An error occurred: {e}")
+        return False
 
 
-def monitor_bitcoin_richest_addresses():
-    # Update if last update is older than 4 hours
-    # Read the last update time from disk
-    last_update_time = pd.read_csv('last_update_time_richest_addresses.csv', header=None, names=['time'])['time'][0]
-    last_update_time = datetime.strptime(last_update_time, '%Y-%m-%d %H:%M:%S')
-
-    if datetime.now() - last_update_time > timedelta(hours=4):
-        process = CrawlerProcess({'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'})
-        process.crawl(BitcoinRichListSpider)
-        process.start()
-
-        # Save the update time to disk
-        now = datetime.now()
-        now_str = now.strftime('%Y-%m-%d %H:%M:%S')
-        pd.DataFrame({'time': [now_str]}).to_csv('last_update_time_richest_addresses.csv', index=False, header=False)
-        print(f'Richest addresses been updated at {now}')
-    else:
-        last_update_time = pd.read_csv('last_update_time_richest_addresses.csv', header=None, names=['time'])['time'][0]
-        last_update_time = datetime.strptime(last_update_time, '%Y-%m-%d %H:%M:%S')
-        print(f'The dataset was already updated less than 4 hours ago at {last_update_time}')
-
-    # Read addresses from the CSV file
-    addresses = read_addresses_from_csv('bitcoin_rich_list2000.csv')
-
-    # Check the total Bitcoin received and sent in the last 24 hours for all addresses
-    total_received, total_sent = check_multiple_addresses(addresses)
-
-    # Print the total Bitcoin received and sent
-    print(f"Total Bitcoin received in the last 24 hours: {total_received / 100000000} BTC")
-    print(f"Total Bitcoin sent in the last 24 hours: {total_sent / 100000000} BTC")
-
-
-monitor_bitcoin_richest_addresses()
+result = check_search_trend(["Bitcoin", "ethereum"], threshold=1.2)
+if result:
+    print("There is a significant increase in search volume for all keywords.")
+else:
+    print("There is no significant increase in search volume for all keywords.")
