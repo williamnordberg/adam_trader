13be46e3e56f4d26c037dc31b74cf6d7ddb99640
Will7i7am7 <william.nordberg3@gmail.com>
2023-03-11 09:49:30 +0800

    first commit

--- main_decision.py
+++ main_decision.py
@@ -1,6 +1,6 @@
 import requests
 from time import sleep
-from old_monitor_2000_richest import monitor_bitcoin_richest_addresses
+from monitor_2000_richest import monitor_bitcoin_richest_addresses
 from order_book import get_probabilities
 from adam_predictor import decision_tree_predictor
 from google_search import check_search_trend
