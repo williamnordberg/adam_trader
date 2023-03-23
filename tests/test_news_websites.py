56b7395a75adf0f8d2c900cbfdb112fb2b26c4a1
Will7i7am7 <william.nordberg3@gmail.com>
2023-03-23 11:02:27 +0800

    first commit

--- tests/test_news_websites.py
+++ tests/test_news_websites.py
@@ -28,6 +28,19 @@ class TestNewsWebsites(unittest.TestCase):
 
     @mock.patch("news_websites.requests.get")
     def test_sentiment_positive(self, mock_get):
+        """
+        Tests the `check_sentiment_of_news` function with a mocked GET request response.
+        The mocked response contains two news articles with opposite sentiment (positive and negative).
+        The function should analyze the sentiment of the news content using a pre-trained machine learning model,
+        and return False since the overall sentiment is positive.
+
+        Args:
+            self: the unittest.TestCase object.
+            mock_get: the mocked requests.get function, which is patched using the @mock.patch decorator.
+
+        Returns:
+            None. The test passes if the check_sentiment_of_news function correctly returns False.
+        """
         mock_get.return_value = self.mock_response
 
         result = check_sentiment_of_news()
