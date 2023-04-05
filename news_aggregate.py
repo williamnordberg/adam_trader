3c006f1f9f057fd4353ba58bdedaa0bae313d3f5
Will7i7am7 <william.nordberg3@gmail.com>
2023-04-05 10:55:54 +0800

    first commit

--- news_aggregate.py
+++ news_aggregate.py
@@ -1,4 +1,3 @@
-import datetime
 from datetime import datetime, timedelta
 
 from newsAPI import check_news_api_sentiment
@@ -9,8 +8,8 @@ from news_scrapper import check_news_sentiment_scrapper
 def aggregate_news():
     # Get aggregated value from 3 function for las 24 hours
 
-    start = datetime.datetime.now() - datetime.timedelta(days=1)
-    end = datetime.datetime.now()
+    start = datetime.now() - timedelta(days=1)
+    end = datetime.now()
     positive_polarity_24_hours_before1, negative_polarity_24_hours_before1, \
         positive_count_24_hours_before1, negative_count_24_hours_before1 = \
         check_news_api_sentiment(start, end)
@@ -23,6 +22,7 @@ def aggregate_news():
         positive_count_24_hours_before3, negative_count_24_hours_before3 = \
         check_news_sentiment_scrapper()
 
+    # divide by number of news aggregates
     positive_polarity_24_hours_before = (positive_polarity_24_hours_before1 + positive_polarity_24_hours_before2
                                          + positive_polarity_24_hours_before3) / 3
 
@@ -34,6 +34,6 @@ def aggregate_news():
 
     negative_count_24_hours_before = (negative_polarity_24_hours_before1 + negative_count_24_hours_before2
                                       + negative_count_24_hours_before3) / 3
+
     return positive_polarity_24_hours_before, negative_polarity_24_hours_before,\
         positive_count_24_hours_before, negative_count_24_hours_before
-
