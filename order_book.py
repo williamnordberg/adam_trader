3f84acf2b12181d0b92a2f55897003732e4f14fa
Will7i7am7 <william.nordberg3@gmail.com>
2023-04-06 14:46:47 +0800

    first commit

--- order_book.py
+++ order_book.py
@@ -26,6 +26,35 @@ def get_order_book(symbol, limit, session):
         return None
 
 
+def compare_probability(probability_up, probability_down):
+
+    if probability_up > probability_down:
+        if probability_up >= 0.65:
+            return 1, 0
+        elif probability_up >= 0.62:
+            return 0.9, 0.1
+        elif probability_up >= 0.59:
+            return 0.8, 0.2
+        elif probability_up >= 0.56:
+            return 0.7, 0.3
+        elif probability_up >= 0.53:
+            return 0.6, 0.4
+
+    elif probability_up < probability_down:
+        if probability_down >= 0.65:
+            return 0, 1
+        elif probability_down >= 0.62:
+            return 0.1, 0.9
+        elif probability_down >= 0.59:
+            return 0.2, 0.8
+        elif probability_down >= 0.56:
+            return 0.3, 0.7
+        elif probability_down >= 0.53:
+            return 0.4, 0.6
+
+    return 0, 0
+
+
 def get_probabilities(symbols, limit=LIMIT, bid_multiplier=0.995, ask_multiplier=1.005):
     bid_volume, ask_volume = 0.0000001, 0
     with requests.Session() as session:
@@ -38,9 +67,12 @@ def get_probabilities(symbols, limit=LIMIT, bid_multiplier=0.995, ask_multiplier
             bid_volume += sum([float(bid[1]) for bid in data['bids'] if float(bid[0]) >= (current_price * bid_multiplier)])
             ask_volume += sum([float(ask[1]) for ask in data['asks'] if float(ask[0]) <= current_price * ask_multiplier])
 
-    probability_down = ask_volume / (bid_volume + ask_volume)
     probability_up = bid_volume / (bid_volume + ask_volume)
-    return probability_down, probability_up
+    probability_down = ask_volume / (bid_volume + ask_volume)
+    print('probability_up, probability_down', probability_up, probability_down)
+    order_book_bullish, order_book_bearish = compare_probability(probability_up, probability_down)
+
+    return order_book_bullish, order_book_bearish
 
 
 def get_probabilities_hit_profit_or_stop(symbols, limit, profit_target, stop_loss):
@@ -60,6 +92,14 @@ def get_probabilities_hit_profit_or_stop(symbols, limit, profit_target, stop_los
 
 
 if __name__ == '__main__':
-    prob_down, prob_up = get_probabilities(SYMBOLS, limit=LIMIT, bid_multiplier=0.995, ask_multiplier=1.005)
-    prob_target, prob_stop_loss = get_probabilities_hit_profit_or_stop(SYMBOLS, LIMIT, 30000, 20000)
-    print(prob_down, prob_up, prob_target, prob_stop_loss)
+    order_book_bullish_outer, order_book_bearish_outer = \
+        get_probabilities(SYMBOLS, limit=LIMIT, bid_multiplier=0.995, ask_multiplier=1.005)
+    order_book_hit_target_outer, order_book_hit_stop_outer = \
+        get_probabilities_hit_profit_or_stop(SYMBOLS, LIMIT, 30000, 20000)
+
+    logging.info(f'order_book_bullish:{order_book_bullish_outer},'
+                 f'order_book_bearish: {order_book_bearish_outer}')
+
+    logging.info(f'order_book_hit_target:{order_book_hit_target_outer},'
+                 f'order_book_hit_stop: {order_book_hit_stop_outer}')
+
