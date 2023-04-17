4639c132be820371c2532ea27cce66e84103fb42
Will7i7am7 <william.nordberg3@gmail.com>
2023-04-17 10:58:40 +0800

    11 April

--- database.py
+++ database.py
@@ -94,7 +94,7 @@ def read_database() -> pd.DataFrame:
     return df
 
 
-def save_value_to_database(column: str, value):
+def save_value_to_database_old(column: str, value):
     """Get the value and column name and save it in the database base of current hour"""
     # Get the current datetime and round it down to the nearest hour
     current_hour = pd.Timestamp.now().floor("H")
@@ -127,5 +127,78 @@ def save_value_to_database(column: str, value):
     df.to_csv('database.csv', index=False)
 
 
+def save_value_to_database_new(column: str, value):
+    """Get the value and column name and save it in the database base of current hour"""
+    # Get the current datetime and round it down to the nearest hour
+    current_hour = pd.Timestamp.now().floor("H")
+
+    df = read_database()
+
+    # Check if there's an existing row for the current hour
+    if current_hour in df.index:
+        # Update the existing row with the new value
+        df.at[current_hour, column] = value
+    else:
+        # Initialize a new row with all columns set to the last non-empty value seen in each column
+        new_row_data: Dict[str, Any] = {}
+        for col in column_names:
+            if col in df.columns:
+                last_valid_value = df[col].dropna().iloc[-1] if not df[col].dropna().empty else None
+                new_row_data[col] = [last_valid_value]
+            else:
+                new_row_data[col] = [None]
+
+        # Set the date and the provided column's value
+        new_row_data["date"] = [current_hour]
+        new_row_data[column] = [value]
+
+        # Create a new DataFrame with the new row data
+        new_row_df = pd.DataFrame(new_row_data)
+        new_row_df.set_index("date", inplace=True)
+
+        # Append the new row DataFrame to the existing DataFrame
+        df = pd.concat([df, new_row_df])
+
+    # Save the updated DataFrame back to the CSV file without the index
+    df.reset_index(inplace=True)
+
+    df.to_csv('database.csv', index=False)
+
+
+def save_value_to_database(column: str, value):
+    """Get the value and column name and save it in the database base of current hour"""
+    # Get the current datetime and round it down to the nearest hour
+    current_hour = pd.Timestamp.now().floor("H")
+
+    df = read_database()
+
+    # Check if there's an existing row for the current hour
+    if current_hour in df.index:
+        # Update the existing row with the new value
+        df.at[current_hour, column] = value
+    else:
+        # Initialize a new row with all columns set to None
+        new_row_data: Dict[str, Any] = {col: [None] for col in column_names}
+
+        # Set the date and the provided column's value
+        new_row_data["date"] = [current_hour]
+        new_row_data[column] = [value]
+
+        # Create a new DataFrame with the new row data
+        new_row_df = pd.DataFrame(new_row_data)
+        new_row_df.set_index("date", inplace=True)
+
+        # Append the new row DataFrame to the existing DataFrame
+        df = pd.concat([df, new_row_df])
+
+        # Fill missing values using the forward-fill method
+        df.fillna(method='ffill', inplace=True)
+
+    # Save the updated DataFrame back to the CSV file without the index
+    df.reset_index(inplace=True)
+
+    df.to_csv('database.csv', index=False)
+
+
 if __name__ == '__main__':
-    save_value_to_database('cpi_m_to_m', 0.12)
+    save_value_to_database('predicted_price', 0.12)
