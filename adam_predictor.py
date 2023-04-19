2f4d074bc26cb10c33d243e26ee673182133ebf2
Will7i7am7 <william.nordberg3@gmail.com>
2023-04-19 08:11:29 +0800

    11 April

--- adam_predictor.py
+++ adam_predictor.py
@@ -13,32 +13,7 @@ from handy_modules import compare_predicted_price, get_bitcoin_price, should_upd
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-DATASET_UPDATE_INTERVAL = timedelta(hours=8)
-LATEST_INFO_FILE = 'latest_info_saved.csv'
 MAIN_DATASET_FILE = 'main_dataset.csv'
-
-
-def should_update_dataset() -> bool:
-    """Check if the dataset should be updated based on the last update time."""
-    latest_info_saved = pd.read_csv(LATEST_INFO_FILE)
-    last_update_time_str = latest_info_saved['latest_dataset_update'][0]
-    last_update_time = datetime.strptime(last_update_time_str, '%Y-%m-%d %H:%M:%S')
-
-    return datetime.now() - last_update_time > DATASET_UPDATE_INTERVAL
-
-
-def update_dataset():
-    """Update the dataset by calling the update functions for different sources."""
-    update_internal_factors()
-    update_yahoo_data()
-    update_macro_economic()
-
-    now = datetime.now()
-    now_str = now.strftime('%Y-%m-%d %H:%M:%S')
-    latest_info_saved = pd.read_csv(LATEST_INFO_FILE)
-    latest_info_saved['latest_dataset_update'] = now_str
-    latest_info_saved.to_csv(LATEST_INFO_FILE, index=False)
-    logging.info(f'dataset has been updated {now}')
 
 
 def load_dataset() -> pd.DataFrame:
@@ -75,8 +50,12 @@ def train_and_predict(dataset: pd.DataFrame) -> int:
 
 def decision_tree_predictor_wrapper() -> Tuple[float, float]:
     """Main function to check if dataset update is needed, load dataset, train model, and make predictions."""
-    if should_update_dataset():
-        update_dataset()
+    if should_update('dataset'):
+        update_internal_factors()
+        update_yahoo_data()
+        update_macro_economic()
+        logging.info('dataset has been updated')
+        save_update_time('dataset')
 
     dataset = load_dataset()
     prediction = train_and_predict(dataset)
