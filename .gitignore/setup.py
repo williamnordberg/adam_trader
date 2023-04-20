c4d905f049a4ce9caa6411b7f016462114f7e599
Will7i7am7 <william.nordberg3@gmail.com>
2023-04-20 15:22:21 +0800

    11 April

--- .gitignore/setup.py
+++ .gitignore/setup.py
@@ -6,7 +6,7 @@ setup(
     author='Will',
     author_email='your.email@example.com',
     description='automation of a trader',
-    long_description=open('README.md').read(),
+    long_description=open('../README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/yourusername/your_project_name',
     packages=find_packages(),
