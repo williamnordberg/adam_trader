2109ebac6fb331a31d9f18d1ff65760212e3c68e
Will7i7am7 <william.nordberg3@gmail.com>
2023-04-20 16:38:00 +0800

    11 April

---
+++ bs4_scraper.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+def scrape_bitcoin_rich_list():
+    base_url = "https://bitinfocharts.com/top-100-richest-bitcoin-addresses"
+    df = pd.DataFrame(columns=["address"])
+
+    for i in range(1, 4):
+        url = f"{base_url}-{i}.html"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        tables = soup.find_all("table")
+
+        for table in tables:
+            data = []
+            for row in table.find_all("tr")[1:]:
+                cells = row.find_all("td")
+                address_link = cells[1].find("a")
+                if address_link:
+                    data.append({
+                        "address": address_link.get_text()
+                    })
+
+            df2 = pd.DataFrame(data)
+            df = pd.concat([df, df2], ignore_index=True)
+
+    df.dropna(subset=["address"], inplace=True)
+    df.to_csv("data/bitcoin_rich_list2000.csv", index=False)
+
+
+if __name__ == "__main__":
+    scrape_bitcoin_rich_list()
