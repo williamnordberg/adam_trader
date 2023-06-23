f1a7ef168639e3214908630a9fcad5fb5259e9ed
Will7i7am7 <william.nordberg3@gmail.com>
2023-06-23 09:23:42 +0800

    22June

--- logging_config.py
+++ logging_config.py
@@ -1,9 +1,10 @@
 import logging
 
-
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s',
-                    handlers=[logging.StreamHandler(), logging.FileHandler('app.log')])
-
+logging.basicConfig(level=logging.INFO, 
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    handlers=[logging.StreamHandler(), logging.FileHandler('app.log')],
+                    datefmt='%Y-%m-%d %H:%M:%S')  # date format without milliseconds
 
 def do_nothing():
     return
+
