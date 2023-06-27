aabdf55bc83821a4dd50a9c48dbbb1844d772468
Will7i7am7 <william.nordberg3@gmail.com>
2023-06-27 15:32:36 +0800

    27June_after change

--- short_position_open.py
+++ short_position_open.py
@@ -7,7 +7,7 @@ from a_macro import macro_sentiment, print_upcoming_events
 from d_technical import technical_analyse
 from news_analyser import check_sentiment_of_news
 from f_google import check_search_trend
-from reddit import reddit_check
+from g_reddit import reddit_check
 from youtube import check_bitcoin_youtube_videos_increase
 from c_predictor import decision_tree_predictor
 from position_decision_maker import position_decision
