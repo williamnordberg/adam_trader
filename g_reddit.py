536755a61a58660e3c5f4a629f164557a1ef7ee4
Will7i7am7 <william.nordberg3@gmail.com>
2023-06-27 16:05:43 +0800

    27June_after change

--- g_reddit.py
+++ g_reddit.py
@@ -1,6 +1,5 @@
 import praw
 import time
-import pandas as pd
 import logging
 import configparser
 from typing import Tuple
@@ -9,15 +8,12 @@ from prawcore.exceptions import RequestException
 from requests.exceptions import SSLError
 from urllib3.exceptions import MaxRetryError
 
-
-from z_database import save_value_to_database
-from handy_modules import save_update_time, should_update, retry_on_error
-from z_database import read_database
+from read_write_csv import save_value_to_database, save_update_time, \
+    should_update, read_latest_data, write_latest_data, retrieve_latest_factor_values_database
+from handy_modules import retry_on_error
 from z_compares import compare_google_reddit_youtube
 
 ONE_DAYS_IN_SECONDS = 24 * 60 * 60
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-LATEST_INFO_SAVED = 'data/latest_info_saved.csv'
 CONFIG_PATH = 'config/config.ini'
 
 
@@ -43,9 +39,10 @@ def count_bitcoin_posts(reddit: Reddit) -> int:
     return count
 
 
-@retry_on_error(
-    max_retries=3, delay=5, allowed_exceptions=(SSLError, MaxRetryError,), fallback_values=(0, 0))
+@retry_on_error(max_retries=3, delay=5, allowed_exceptions=(
+        SSLError, MaxRetryError,), fallback_values=(0.0, 0.0))
 def reddit_check_wrapper() -> Tuple[float, float]:
+    save_update_time('reddit')
 
     config = configparser.ConfigParser()
     config.read(CONFIG_PATH)
@@ -56,23 +53,16 @@ def reddit_check_wrapper() -> Tuple[float, float]:
         client_secret=reddit_config['client_secret'],
         user_agent=reddit_config['user_agent']
     )
+    previous_activity = read_latest_data('previous_activity', int)
+    # read_latest_data('previous_count', int)
 
-    latest_info_saved = pd.read_csv(LATEST_INFO_SAVED).squeeze("columns")
-
-    previous_activity = float(latest_info_saved['previous_activity'][0])
-    # previous_count = float(latest_info_saved['previous_count'][0])
     try:
         current_activity = reddit.subreddit("Bitcoin").active_user_count
         current_count = count_bitcoin_posts(reddit)
         reddit_bullish, reddit_bearish = compare_google_reddit_youtube(int(current_activity), int(previous_activity))
 
-        latest_info_saved.loc[0, 'previous_activity'] = current_activity
-        latest_info_saved.loc[0, 'previous_count'] = current_count
-
-        latest_info_saved.to_csv(LATEST_INFO_SAVED, index=False)
-
-        # Save latest update time
-        save_update_time('reddit')
+        write_latest_data('previous_activity', current_activity)
+        write_latest_data('previous_count', current_count)
 
         # Save to database
         save_value_to_database('reddit_bullish', reddit_bullish)
@@ -90,10 +80,7 @@ def reddit_check() -> Tuple[float, float]:
     if should_update('reddit'):
         return reddit_check_wrapper()
     else:
-        database = read_database()
-        reddit_bullish = database['reddit_bullish'][-1]
-        reddit_bearish = database['reddit_bearish'][-1]
-        return reddit_bullish, reddit_bearish
+        return retrieve_latest_factor_values_database('reddit')
 
 
 if __name__ == '__main__':
