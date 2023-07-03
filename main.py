fd1ce5eeb581fe0a3552a18ae20e51cdbdbe7f26
will7i7am7 <william.nordberg3@gmail.com>
2023-07-03 13:10:04 +0800

    27June_after change

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def run_monitor_richest_addresses():
             save_value_to_database('richest_addresses_bearish', richest_addresses_bearish)
 
             logging.info(f'Richest addresses Send: {total_sent} '
-                         f' RECEIVE: {total_received} in 24 H')
+                         f' RECEIVE: {total_received} in    24 H')
         sleep(RICHEST_ADDRESSES_SLEEP_TIME)
 
 
