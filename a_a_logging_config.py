e9c990a24a0cd5eee2f1b9a88feeca0b3a9c9bb4
will7i7am7 <william.nordberg3@gmail.com>
2023-07-04 11:01:21 +0800

    27June_after change

--- a_a_logging_config.py
+++ a_a_logging_config.py
@@ -1,8 +1,12 @@
 import logging
 
 logging.getLogger('google.auth').setLevel(logging.ERROR)
-logging.basicConfig(level=logging.INFO, 
-                    format='%(asctime)s - %(levelname)s - %(message)s',
+logging.getLogger('google_auth_httplib2').setLevel(logging.WARNING)
+logging.getLogger('oauth2client').setLevel(logging.WARNING)
+logging.getLogger('googleapiclient.discovery_cache').setLevel(logging.ERROR)
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
                     handlers=[logging.StreamHandler(), logging.FileHandler('logs/app.log')],
                     datefmt='%Y-%m-%d %H:%M:%S')  # date format without milliseconds
 
