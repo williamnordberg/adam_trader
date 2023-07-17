e6a5f261d57d73ab0ab2ad0b77d94ea5fef36e32
will7i7am7 <william.nordberg3@gmail.com>
2023-07-17 10:30:26 +0800

    16July

--- b_order_book.py
+++ b_order_book.py
@@ -13,6 +13,9 @@ FUTURES_ENDPOINT_DEPTH = 'https://fapi.binance.com/fapi/v1/depth'
 LIMIT = 1000
 SYMBOLS = ['BTCUSDT', 'BTCBUSD']
 
+BID_MULTIPLIER = 0.99
+ASK_MULTIPLIER = 1.01
+
 
 @retry_on_error(max_retries=3, delay=5,
                 allowed_exceptions=(requests.exceptions.RequestException, KeyError, TypeError
@@ -29,8 +32,8 @@ def get_volume(symbol: str, limit: int) -> dict:
     return response.json()
 
 
-def order_book(symbols: List[str], limit: int = LIMIT, bid_multiplier: float = 0.995,
-               ask_multiplier: float = 1.005) -> Tuple[float, float]:
+def order_book(symbols: List[str], limit: int = LIMIT, bid_multiplier: float = BID_MULTIPLIER,
+               ask_multiplier: float = ASK_MULTIPLIER) -> Tuple[float, float]:
 
     bid_volume, ask_volume = 0.0000001, 0.0
     current_price = get_bitcoin_price()
@@ -48,8 +51,8 @@ def order_book(symbols: List[str], limit: int = LIMIT, bid_multiplier: float = 0
 
     order_book_bullish, order_book_bearish = compare_order_volume(probability_up, probability_down)
 
-    save_value_to_database('bid_volume', round(bid_volume, 2))
-    save_value_to_database('ask_volume', round(ask_volume, 2))
+    save_value_to_database('bid_volume', round(bid_volume, 0))
+    save_value_to_database('ask_volume', round(ask_volume, 0))
     save_value_to_database('order_book_bullish', order_book_bullish)
     save_value_to_database('order_book_bearish', order_book_bearish)
 
@@ -87,6 +90,6 @@ def order_book_hit_target(symbols: List[str], limit: int, profit_target: float,
 
 if __name__ == '__main__':
     order_book_bullish_outer, order_book_bearish_outer = order_book(
-        SYMBOLS, limit=LIMIT, bid_multiplier=0.99, ask_multiplier=1.01)
+        SYMBOLS, limit=LIMIT, bid_multiplier=BID_MULTIPLIER, ask_multiplier=ASK_MULTIPLIER)
     logging.info(f'order_book_bullish:{order_book_bullish_outer},'
                  f'order_book_bearish: {order_book_bearish_outer}')
