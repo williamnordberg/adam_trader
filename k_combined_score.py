123f4849fe86a96b929995e0302835416dc676b1
will7i7am7 <william.nordberg3@gmail.com>
2023-08-01 15:04:38 +0800

    1august

--- k_combined_score.py
+++ k_combined_score.py
@@ -1,121 +1,92 @@
+from a_a_logging_config import do_nothing
 import logging
-from typing import Dict, List, Tuple
+from typing import Tuple
 from z_read_write_csv import save_value_to_database, save_update_time
 
-MIN_CONTRIBUTING_FACTORS = 4
-
-# Temporary storage for aggregated values
-aggregated_values: Dict[str, List[float]] = {
-    'weighted_score_up': [],
-    'weighted_score_down': []
+MIN_CONTRIBUTING_FACTORS_WEIGHT = 0.6
+WEIGHTS = {
+    "macro_indicators": 0.2,
+    "order_book": 0.2,
+    "predicted_price": 0.15,
+    "technical_analysis": 0.125,
+    "richest_addresses": 0.125,
+    "google_search": 0.05,
+    "reddit": 0.05,
+    "youtube": 0.05,
+    "sentiment_of_news": 0.05,
 }
+do_nothing()
 
 
-def count_contributing_factors(*factors: Tuple[float, float]) -> int:
-    count = 0
-    for bullish, bearish in factors:
+def count_contributing_factors(*factors: Tuple[float, float]) -> float:
+    total_weight = 0.0
+    for weight, (bullish, bearish) in zip(WEIGHTS.values(), factors):
         if bullish != 0 or bearish != 0:
-            count += 1
-    return count
+            total_weight += weight
+    return total_weight
 
 
 def make_trading_decision(factor_values) -> Tuple[float, float]:
-    """
-    Makes a trading decision based on the conditions met.
-    """
-
-    macro_bullish = factor_values['macro_bullish']
-    macro_bearish = factor_values['macro_bearish']
-    order_book_bullish = factor_values['order_book_bullish']
-    order_book_bearish = factor_values['order_book_bearish']
-    prediction_bullish = factor_values['prediction_bullish']
-    prediction_bearish = factor_values['prediction_bearish']
-    technical_bullish = factor_values['technical_bullish']
-    technical_bearish = factor_values['technical_bearish']
-    richest_addresses_bullish = factor_values['richest_bullish']
-    richest_addresses_bearish = factor_values['richest_bearish']
-    google_search_bullish = factor_values['google_bullish']
-    google_search_bearish = factor_values['google_bearish']
-    reddit_bullish = factor_values['reddit_bullish']
-    reddit_bearish = factor_values['reddit_bearish']
-    youtube_bullish = factor_values['youtube_bullish']
-    youtube_bearish = factor_values['youtube_bearish']
-    news_bullish = factor_values['news_bullish']
-    news_bearish = factor_values['news_bearish']
-
-    # Calculate the number of contributing factors
-    num_contributing_factors = count_contributing_factors(
-        (macro_bullish, macro_bearish),
-        (order_book_bullish, order_book_bearish),
-        (prediction_bullish, prediction_bearish),
-        (technical_bullish, technical_bearish),
-        (richest_addresses_bullish, richest_addresses_bearish),
-        (google_search_bullish, google_search_bearish),
-        (reddit_bullish, reddit_bearish),
-        (youtube_bullish, youtube_bearish),
-        (news_bullish, news_bearish)
+    factors = (
+        (factor_values['macro_bullish'], factor_values['macro_bearish']),
+        (factor_values['order_book_bullish'], factor_values['order_book_bearish']),
+        (factor_values['prediction_bullish'], factor_values['prediction_bearish']),
+        (factor_values['technical_bullish'], factor_values['technical_bearish']),
+        (factor_values['richest_bullish'], factor_values['richest_bearish']),
+        (factor_values['google_bullish'], factor_values['google_bearish']),
+        (factor_values['reddit_bullish'], factor_values['reddit_bearish']),
+        (factor_values['youtube_bullish'], factor_values['youtube_bearish']),
+        (factor_values['news_bullish'], factor_values['news_bearish'])
     )
 
-    weights = {
-        "macro_indicators": 0.2,
-        "order_book": 0.2,
-        "predicted_price": 0.15,
-        "technical_analysis": 0.125,
-        "richest_addresses": 0.125,
-        "google_search": 0.05,
-        "reddit": 0.05,
-        "youtube": 0.05,
-        "sentiment_of_news": 0.05,
-    }
-
-    # Calculate the weighted score for each alternative:
-    weighted_score_up = (
-            weights["macro_indicators"] * macro_bullish +
-            weights["order_book"] * order_book_bullish +
-            weights["predicted_price"] * prediction_bullish +
-            weights["technical_analysis"] * technical_bullish +
-            weights["richest_addresses"] * richest_addresses_bullish +
-            weights["google_search"] * google_search_bullish +
-            weights["reddit"] * reddit_bullish +
-            weights["youtube"] * youtube_bullish +
-            weights["sentiment_of_news"] * news_bullish
-    )
-
-    weighted_score_down = (
-            weights["macro_indicators"] * macro_bearish +
-            weights["order_book"] * order_book_bearish +
-            weights["predicted_price"] * prediction_bearish +
-            weights["technical_analysis"] * technical_bearish +
-            weights["richest_addresses"] * richest_addresses_bearish +
-            weights["google_search"] * google_search_bearish +
-            weights["reddit"] * reddit_bearish +
-            weights["youtube"] * youtube_bearish +
-            weights["sentiment_of_news"] * news_bearish
-    )
+    weight_of_contributing_factors = count_contributing_factors(*factors)
+    print('weight_of_contributing_factors', weight_of_contributing_factors )
+    weighted_score_up = weighted_score_down = 0
+    for factor, (bullish, bearish) in zip(WEIGHTS.values(), factors):
+        weighted_score_up += factor * bullish
+        weighted_score_down += factor * bearish
 
     save_update_time('weighted_score')
 
-    # Check if the minimum number of contributing factors is met
-    if num_contributing_factors >= MIN_CONTRIBUTING_FACTORS:
-        # Normalize the scores
-        total_score = weighted_score_up + weighted_score_down
-
-        normalized_score_up = round((weighted_score_up / total_score), 2)
-        normalized_score_down = round((weighted_score_down / total_score), 2)
-
-        # Save the latest weighted score
-        save_value_to_database('weighted_score_up', normalized_score_up)
-        save_value_to_database('weighted_score_down', normalized_score_down)
-
-        return normalized_score_up, normalized_score_down
-    else:
-        # Save the latest weighted score
+    if weight_of_contributing_factors < MIN_CONTRIBUTING_FACTORS_WEIGHT:
+        logging.info(f"Minimum contributing factors weight not met: "
+                     f"{weight_of_contributing_factors}/ out of {MIN_CONTRIBUTING_FACTORS_WEIGHT}")
         save_value_to_database('weighted_score_up', 0.0)
-        save_value_to_database('weighted_score_down',  0.0)
-
-        logging.info(f"Minimum contributing factors not met: {num_contributing_factors}/{MIN_CONTRIBUTING_FACTORS}")
+        save_value_to_database('weighted_score_down', 0.0)
         return 0.0, 0.0
+
+    print('weighted_score_up, weighted_score_down', weighted_score_up, weighted_score_down)
+    total_score = weighted_score_up + weighted_score_down
+    normalized_score_up = round((weighted_score_up / total_score), 2)
+    normalized_score_down = round((weighted_score_down / total_score), 2)
+
+    save_value_to_database('weighted_score_up', normalized_score_up)
+    save_value_to_database('weighted_score_down', normalized_score_down)
+
+    return normalized_score_up, normalized_score_down
 
 
 if __name__ == "__main__":
-    score_up, score_down = 0, 0
+    factor_values_outer = {
+        'macro_bullish': 1,
+        'macro_bearish': 0.0,
+        'order_book_bullish': 0.0,
+        'order_book_bearish': 0.0,
+        'prediction_bullish': 0.0,
+        'prediction_bearish': 0.0,
+        'technical_bullish': 1,
+        'technical_bearish': 0.0,
+        'richest_bullish': 0.0,
+        'richest_bearish': 1,
+        'google_bullish': 0.0,
+        'google_bearish': 1,
+        'reddit_bullish': 0.0,
+        'reddit_bearish': 0.0,
+        'youtube_bullish': 1,
+        'youtube_bearish': 0.0,
+        'news_bullish': 0.0,
+        'news_bearish': 0.0,
+        'weighted_score_up': 0.0,
+        'weighted_score_down': 0.0
+    }
+    print(make_trading_decision(factor_values_outer))
