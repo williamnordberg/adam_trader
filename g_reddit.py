a15779e0d7397aaccef2a5fdc9996022972995bc
will7i7am7 <william.nordberg3@gmail.com>
2023-08-01 10:24:53 +0800

    1august

--- g_reddit.py
+++ g_reddit.py
@@ -1,19 +1,81 @@
+import pandas as pd
+from textblob import TextBlob
 import praw
-import logging
 import configparser
 from typing import Tuple
 from requests.exceptions import SSLError
 from urllib3.exceptions import MaxRetryError
 from prawcore.exceptions import RequestException
 
-
 from z_read_write_csv import save_value_to_database, save_update_time, \
-    should_update, read_latest_data, write_latest_data, retrieve_latest_factor_values_database
+    should_update, retrieve_latest_factor_values_database, read_database
 from z_handy_modules import retry_on_error
-from z_compares import compare_google_reddit_youtube
 
-ONE_DAYS_IN_SECONDS = 24 * 60 * 60
+
+SHORT_MOVING_AVERAGE_WINDOW = 5
+LONG_MOVING_AVERAGE_WINDOW = 20
 CONFIG_PATH = 'config/config.ini'
+
+
+def calculate_reddit_trend(reddit_bullish, reddit_bearish, short_ma, long_ma, factor_type):
+    adjustment = 0.125
+
+    if factor_type == 'positive_polarity' or factor_type == 'positive_count':
+        if short_ma > long_ma:
+            reddit_bullish += adjustment
+            reddit_bearish -= adjustment
+    elif factor_type == 'negative_polarity' or factor_type == 'negative_count':
+        if short_ma > long_ma:
+            reddit_bullish -= adjustment
+            reddit_bearish += adjustment
+
+    return reddit_bullish, reddit_bearish
+
+
+def calculate_reddit_sentiments():
+    reddit_bullish, reddit_bearish = 0.5, 0.5
+    data = read_database()
+    df = pd.DataFrame(data, columns=['reddit_positive_polarity',
+                                     'reddit_negative_polarity', 'reddit_positive_count',
+                                     'reddit_negative_count'])
+
+    for col in df.columns:
+        short_ma = df[col].rolling(window=SHORT_MOVING_AVERAGE_WINDOW, min_periods=1).mean()
+        long_ma = df[col].rolling(window=LONG_MOVING_AVERAGE_WINDOW, min_periods=1).mean()
+        reddit_bullish, reddit_bearish = calculate_reddit_trend(
+            reddit_bullish, reddit_bearish, short_ma.iloc[-1], long_ma.iloc[-1], col)
+    return reddit_bullish, reddit_bearish
+
+
+def calculate_sentiment_score(content: str) -> float:
+    blob = TextBlob(content)
+    return blob.sentiment.polarity
+
+
+def preprocess_and_save_reddit_data(reddit, subreddit_name):
+    positive_polarity_score, negative_polarity_score = 0.0, 0.0
+    positive_count, negative_count = 0, 0
+    subreddit = reddit.subreddit(subreddit_name)
+
+    for post in subreddit.hot(limit=50):
+        content = post.title + " " + post.selftext
+        sentiment_score = calculate_sentiment_score(content)
+        if sentiment_score > 0:
+            positive_polarity_score += sentiment_score
+            positive_count += 1
+        elif sentiment_score < 0:
+            negative_polarity_score += sentiment_score
+            negative_count += 1
+
+    # Calculate average positive and negative sentiments
+    positive_polarity = positive_polarity_score / positive_count if positive_count != 0 else 0
+    negative_polarity = abs(negative_polarity_score / negative_count) if negative_count != 0 else 0
+
+    # save all value in database to calculate later
+    save_value_to_database('reddit_positive_polarity', positive_polarity)
+    save_value_to_database('reddit_negative_polarity', negative_polarity)
+    save_value_to_database('reddit_positive_count', positive_count)
+    save_value_to_database('reddit_negative_count', negative_count)
 
 
 @retry_on_error(max_retries=3, delay=5, allowed_exceptions=(
@@ -29,16 +91,19 @@ def reddit_check_wrapper() -> Tuple[float, float]:
         client_secret=reddit_config['client_secret'],
         user_agent=reddit_config['user_agent']
     )
-    previous_activity = read_latest_data('reddit_previous_activity', int)
 
+    # We save this just for visualization
     current_activity = reddit.subreddit("Bitcoin").active_user_count
-    reddit_bullish, reddit_bearish = compare_google_reddit_youtube(int(current_activity), int(previous_activity))
-
-    write_latest_data('reddit_previous_activity', current_activity)
-
     save_value_to_database('reddit_activity_24h', current_activity)
 
+    preprocess_and_save_reddit_data(reddit, "Bitcoin")
+
+    reddit_bullish, reddit_bearish = calculate_reddit_sentiments()
+
     save_update_time('reddit')
+
+    if reddit_bullish == 0.5 and reddit_bearish == 0.5:
+        reddit_bullish, reddit_bearish = 0, 0
 
     return reddit_bullish, reddit_bearish
 
@@ -46,16 +111,14 @@ def reddit_check_wrapper() -> Tuple[float, float]:
 def reddit_check() -> Tuple[float, float]:
     if should_update('reddit'):
         reddit_bullish, reddit_bearish = reddit_check_wrapper()
-
-        # Save to database
         save_value_to_database('reddit_bullish', reddit_bullish)
         save_value_to_database('reddit_bearish', reddit_bearish)
-
         return reddit_bullish, reddit_bearish
+
     else:
         return retrieve_latest_factor_values_database('reddit')
 
 
 if __name__ == '__main__':
     reddit_bullish_outer, reddit_bearish_outer = reddit_check_wrapper()
-    logging.info(f", reddit_bullish: {reddit_bullish_outer}, reddit_bearish: {reddit_bearish_outer}")
+    print(f"reddit_bullish: {reddit_bullish_outer}, reddit_bearish: {reddit_bearish_outer}")
--- test.py
+++ test.py
@@ -1,121 +1,50 @@
 import pandas as pd
-import google_auth_oauthlib.flow
-import googleapiclient.discovery
-import googleapiclient.errors
-from googleapiclient.errors import UnknownApiNameOrVersion
 from textblob import TextBlob
-import pickle
-import os.path
-from google.auth.transport.requests import Request
-from dateutil.parser import parse
-from datetime import datetime, timedelta, timezone
+import praw
+import configparser
 from typing import Tuple
-from googleapiclient.errors import HttpError
-from google.auth.exceptions import RefreshError
+from requests.exceptions import SSLError
+from urllib3.exceptions import MaxRetryError
+from prawcore.exceptions import RequestException
 
+from z_read_write_csv import save_value_to_database, save_update_time, \
+    should_update, retrieve_latest_factor_values_database, read_database
 from z_handy_modules import retry_on_error
-from z_read_write_csv import save_value_to_database, \
-    should_update, save_update_time, retrieve_latest_factor_values_database, read_database
 
-SENTIMENT_POSITIVE_THRESHOLD = 0.1
-SENTIMENT_NEGATIVE_THRESHOLD = -0.001
+
 SHORT_MOVING_AVERAGE_WINDOW = 5
 LONG_MOVING_AVERAGE_WINDOW = 20
+CONFIG_PATH = 'config/config.ini'
 
 
-def calculate_youtube_sentiments(youtube_bullish, youtube_bearish, short_ma, long_ma, factor_type):
+def calculate_reddit_trend(reddit_bullish, reddit_bearish, short_ma, long_ma, factor_type):
     adjustment = 0.125
 
     if factor_type == 'positive_polarity' or factor_type == 'positive_count':
         if short_ma > long_ma:
-            youtube_bullish += adjustment
-            youtube_bearish -= adjustment
+            reddit_bullish += adjustment
+            reddit_bearish -= adjustment
     elif factor_type == 'negative_polarity' or factor_type == 'negative_count':
         if short_ma > long_ma:
-            youtube_bullish -= adjustment
-            youtube_bearish += adjustment
+            reddit_bullish -= adjustment
+            reddit_bearish += adjustment
 
-    return youtube_bullish, youtube_bearish
+    return reddit_bullish, reddit_bearish
 
 
-def calculate_bitcoin_youtube_videos_increase():
-    youtube_bullish, youtube_bearish = 0.5, 0.5
+def calculate_reddit_sentiments():
+    reddit_bullish, reddit_bearish = 0.5, 0.5
     data = read_database()
-    df = pd.DataFrame(data, columns=['youtube_positive_polarity',
-                                     'youtube_negative_polarity', 'youtube_positive_count',
-                                     'youtube_negative_count'])
+    df = pd.DataFrame(data, columns=['reddit_positive_polarity',
+                                     'reddit_negative_polarity', 'reddit_positive_count',
+                                     'reddit_negative_count'])
 
     for col in df.columns:
         short_ma = df[col].rolling(window=SHORT_MOVING_AVERAGE_WINDOW, min_periods=1).mean()
         long_ma = df[col].rolling(window=LONG_MOVING_AVERAGE_WINDOW, min_periods=1).mean()
-        youtube_bullish, youtube_bearish = calculate_youtube_sentiments(
-            youtube_bullish, youtube_bearish, short_ma.iloc[-1], long_ma.iloc[-1], col)
-
-    return youtube_bullish, youtube_bearish
-
-
-def calculate_youtube_temporal_decay(sentiment_score: float, publish_time: datetime) -> float:
-    current_time = datetime.now(tz=timezone.utc)
-    hours_diff = (current_time - publish_time).total_seconds() / 3600
-
-    base = 0.9  # decay factor at 24 hours
-    half_life = 24  # half-life in hours
-    decay_factor = base ** (hours_diff / half_life)
-
-    return sentiment_score * decay_factor
-
-
-@retry_on_error(max_retries=3, delay=5, allowed_exceptions=(
-        FileNotFoundError, pickle.UnpicklingError,
-        RefreshError, HttpError, UnknownApiNameOrVersion), fallback_values='pass')
-def get_authenticated_service():
-    api_service_name = "youtube"
-    api_version = "v3"
-    client_secrets_file = "config/youtube_client_secret.json"
-    scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
-
-    creds = None
-    token_file = 'config/youtube_token.pickle'
-    if os.path.exists(token_file):
-        with open(token_file, 'rb') as token:
-            creds = pickle.load(token)
-
-    if not creds or not creds.valid:
-
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-                client_secrets_file, scopes)
-            creds = flow.run_local_server(port=0)
-
-    with open(token_file, 'wb') as token:
-        pickle.dump(creds, token)
-
-    youtube = googleapiclient.discovery.build(api_service_name, api_version, credentials=creds)
-    return youtube
-
-
-@retry_on_error(max_retries=3, delay=5, allowed_exceptions=(HttpError, RefreshError), fallback_values='pass')
-def get_youtube_videos(youtube, published_after, published_before):
-    search_request = youtube.search().list(
-        part="id,snippet",
-        q="#bitcoin",
-        type="video",
-        videoDefinition="high",
-        videoDuration="short",
-        videoDimension="2d",
-        publishedAfter=published_after,
-        publishedBefore=published_before,
-        maxResults=50
-    )
-
-    search_results = []
-    while search_request:
-        search_response = search_request.execute()
-        search_results.extend(search_response['items'])
-        search_request = youtube.search().list_next(search_request, search_response)
-    return search_results
+        reddit_bullish, reddit_bearish = calculate_reddit_trend(
+            reddit_bullish, reddit_bearish, short_ma.iloc[-1], long_ma.iloc[-1], col)
+    return reddit_bullish, reddit_bearish
 
 
 def calculate_sentiment_score(content: str) -> float:
@@ -123,78 +52,73 @@ def calculate_sentiment_score(content: str) -> float:
     return blob.sentiment.polarity
 
 
-def calculate_sentiment_youtube_videos(youtube, published_after, published_before):
-    positive_polarity_score, negative_polarity_score, positive_count, negative_count = 0.0, 0.0, 0, 0
-    search_results = get_youtube_videos(youtube, published_after, published_before)
+def preprocess_and_save_reddit_data(reddit, subreddit_name):
+    positive_polarity_score, negative_polarity_score = 0.0, 0.0
+    positive_count, negative_count = 0, 0
+    subreddit = reddit.subreddit(subreddit_name)
 
-    for video in search_results:
-        title = video['snippet']['title']
-        description = video['snippet']['description']
-        content = title + " " + description
-
+    for post in subreddit.hot(limit=50):
+        content = post.title + " " + post.selftext
         sentiment_score = calculate_sentiment_score(content)
-        publish_time = parse(video['snippet']['publishedAt'])
-        # Applying temporal decay
-        sentiment_score = calculate_youtube_temporal_decay(sentiment_score, publish_time)
-
-        if sentiment_score > SENTIMENT_POSITIVE_THRESHOLD:
+        if sentiment_score > 0:
             positive_polarity_score += sentiment_score
             positive_count += 1
-
-        elif sentiment_score < SENTIMENT_NEGATIVE_THRESHOLD:
+        elif sentiment_score < 0:
             negative_polarity_score += sentiment_score
             negative_count += 1
 
-    positive_sentiment = positive_polarity_score / positive_count if positive_count != 0 else 0
-    negative_sentiment = abs(negative_polarity_score / negative_count) if negative_count != 0 else 0
+    # Calculate average positive and negative sentiments
+    positive_polarity = positive_polarity_score / positive_count if positive_count != 0 else 0
+    negative_polarity = abs(negative_polarity_score / negative_count) if negative_count != 0 else 0
 
-    return positive_sentiment, negative_sentiment, positive_count, negative_count
-
-
-@retry_on_error(
-    max_retries=3, delay=5, allowed_exceptions=(RefreshError,),
-    fallback_values=(0.0, 0.0))
-def youtube_wrapper() -> Tuple[float, float]:
-    youtube = get_authenticated_service()
-
-    now = datetime.utcnow()
-    last_24_hours_start = (now - timedelta(days=1)).strftime('%Y-%m-%dT%H:%M:%SZ')
-    last_24_hours_end = now.strftime('%Y-%m-%dT%H:%M:%SZ')
-
-    positive_polarity, negative_polarity, positive_count, negative_count = \
-        calculate_sentiment_youtube_videos(youtube, last_24_hours_start, last_24_hours_end)
-
-    # Save to database
-    save_value_to_database('last_24_youtube', positive_count + negative_count)
-    save_value_to_database('youtube_positive_polarity', positive_polarity)
-    save_value_to_database('youtube_negative_polarity', negative_polarity)
-    save_value_to_database('youtube_positive_count', positive_count)
-    save_value_to_database('youtube_negative_count', negative_count)
-
-    youtube_bullish, youtube_bearish = calculate_bitcoin_youtube_videos_increase()
-    save_update_time('youtube')
-    print('positive_count, negative_count', positive_polarity, negative_polarity)
-    print('positive_count, negative_count', positive_count, negative_count)
-    print('youtube_bullish, youtube_bearish', youtube_bullish, youtube_bearish)
-
-    return youtube_bullish, youtube_bearish
+    # save all value in database to calculate later
+    save_value_to_database('reddit_positive_polarity', positive_polarity)
+    save_value_to_database('reddit_negative_polarity', negative_polarity)
+    save_value_to_database('reddit_positive_count', positive_count)
+    save_value_to_database('reddit_negative_count', negative_count)
 
 
-def check_bitcoin_youtube_videos_increase() -> Tuple[float, float]:
-    if should_update('youtube'):
+@retry_on_error(max_retries=3, delay=5, allowed_exceptions=(
+        SSLError, MaxRetryError, RequestException), fallback_values=(0.0, 0.0))
+def reddit_check_wrapper() -> Tuple[float, float]:
 
-        youtube_bullish, youtube_bearish = youtube_wrapper()
-        if youtube_bullish == 0.5 and youtube_bearish == 0.5:
-            youtube_bullish, youtube_bearish = 0, 0
+    config = configparser.ConfigParser()
+    config.read(CONFIG_PATH)
 
-        save_value_to_database('youtube_bullish', youtube_bullish)
-        save_value_to_database('youtube_bearish', youtube_bearish)
+    reddit_config = config['reddit']
+    reddit = praw.Reddit(
+        client_id=reddit_config['client_id'],
+        client_secret=reddit_config['client_secret'],
+        user_agent=reddit_config['user_agent']
+    )
 
-        return youtube_bullish, youtube_bearish
+    # We save this just for visualization
+    current_activity = reddit.subreddit("Bitcoin").active_user_count
+    save_value_to_database('reddit_activity_24h', current_activity)
+
+    preprocess_and_save_reddit_data(reddit, "Bitcoin")
+
+    reddit_bullish, reddit_bearish = calculate_reddit_sentiments()
+
+    save_update_time('reddit')
+
+    if reddit_bullish == 0.5 and reddit_bearish == 0.5:
+        reddit_bullish, reddit_bearish = 0, 0
+
+    return reddit_bullish, reddit_bearish
+
+
+def reddit_check() -> Tuple[float, float]:
+    if should_update('reddit'):
+        reddit_bullish, reddit_bearish = reddit_check_wrapper()
+        save_value_to_database('reddit_bullish', reddit_bullish)
+        save_value_to_database('reddit_bearish', reddit_bearish)
+        return reddit_bullish, reddit_bearish
+
     else:
-        return retrieve_latest_factor_values_database('youtube')
+        return retrieve_latest_factor_values_database('reddit')
 
 
-if __name__ == "__main__":
-    youtube_bullish_outer, youtube_bearish_outer = youtube_wrapper()
-    print(f'youtube_bullish: {youtube_bullish_outer} , youtube_bearish: {youtube_bearish_outer}')
+if __name__ == '__main__':
+    reddit_bullish_outer, reddit_bearish_outer = reddit_check_wrapper()
+    print(f"reddit_bullish: {reddit_bullish_outer}, reddit_bearish: {reddit_bearish_outer}")
