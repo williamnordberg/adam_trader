b35a2296592b75ad88c116404b9a64c1cff6efa0
will7i7am7 <william.nordberg3@gmail.com>
2023-08-22 15:54:42 +0800

    19aug

--- m_visualization_divs.py
+++ m_visualization_divs.py
@@ -119,11 +119,6 @@ def create_widget():
             style={'display': 'flex', 'justifyContent': 'center', 'alignItems': 'center', 'marginTop': '10px'}),
 
         html.Div(children=[
-            html.Iframe(src='/assets/stock_heatmap.html', style={'width': '90%', 'height': '300px'}),
-        ],
-            style={'display': 'flex', 'justifyContent': 'center', 'alignItems': 'center', 'marginTop': '5px'}),
-
-        html.Div(children=[
             html.Iframe(src='/assets/cryptopanic.html', style={'width': '30%', 'height': '400px'}),
             html.Iframe(src='/assets/crypto_heatmap.html', style={'width': '30%', 'height': '400px'}),
         ], style={'display': 'flex', 'justifyContent': 'space-between', 'margin': '0 5%'}),
