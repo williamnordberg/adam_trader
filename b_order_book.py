3f8874f73e9b061fcbab9d31a5184a9e9393341b
will7i7am7 <william.nordberg3@gmail.com>
2023-09-18 10:43:59 +0800

    15sep

--- b_order_book.py
+++ b_order_book.py
@@ -5,14 +5,12 @@ from z_read_write_csv import save_update_time, read_latest_data,\
     write_latest_data, save_value_to_database
 from z_handy_modules import get_bitcoin_price, retry_on_error
 from z_compares import compare_order_volume
-from a_config import position, SHORT_POSITION
-from z_handy_modules import get_bitcoin_future_market_price
 
 
 SPOT_ENDPOINT_DEPTH = "https://api.binance.com/api/v3/depth"
 FUTURES_ENDPOINT_DEPTH = 'https://fapi.binance.com/fapi/v1/depth'
 
-LIMIT = 1000
+LIMIT = 5000
 SYMBOLS = ['BTCUSDT', 'BTCBUSD']
 
 BID_MULTIPLIER = 0.99
@@ -42,13 +40,16 @@ def order_book(symbols: List[str], limit: int = LIMIT, bid_multiplier: float = B
     for symbol in symbols:
         data = get_volume(symbol, limit)
         if data is None:
-            print('data is not None')
             return 0.5
-        print('data\n ', data)
-        bid_volume += sum([float(bid[1]) for bid in data['bids'] if float(bid[0]) >=
-                           (current_price * bid_multiplier)])
-        ask_volume += sum([float(ask[1]) for ask in data['asks'] if float(ask[0]) <=
-                           current_price * ask_multiplier])
+
+        try:
+            bid_volume += sum([float(bid[1]) for bid in data['bids'] if float(bid[0]) >=
+                               (current_price * bid_multiplier)])
+            ask_volume += sum([float(ask[1]) for ask in data['asks'] if float(ask[0]) <=
+                               current_price * ask_multiplier])
+        except:
+            bid_volume, ask_volume = 0, 0
+            print(f'Exception as {data}')
 
     probability_up = bid_volume / (bid_volume + ask_volume)
     probability_down = ask_volume / (bid_volume + ask_volume)
@@ -67,8 +68,6 @@ def order_book(symbols: List[str], limit: int = LIMIT, bid_multiplier: float = B
 
 def order_book_hit_target(symbols: List[str], limit: int, profit_target: float,
                           stop_loss: float) -> float:
-    print('here pull worked \n', )
-
     bid_volume, ask_volume = 0.0000001, 0.0
     for symbol in symbols:
         data = get_volume(symbol, limit)
@@ -93,20 +92,6 @@ def order_book_hit_target(symbols: List[str], limit: int, profit_target: float,
 
 
 if __name__ == '__main__':
-    print('order_book')
     order_book_bullish_outer = order_book(
         SYMBOLS, limit=LIMIT, bid_multiplier=BID_MULTIPLIER, ask_multiplier=ASK_MULTIPLIER)
     print(f'order_book_bullish:{order_book_bullish_outer}')
-
-    print('order_book_ target')
-
-    current_price = get_bitcoin_future_market_price()
-
-    profit_target = int(current_price - (current_price * SHORT_POSITION['PROFIT_MARGIN']))
-    stop_loss = int(current_price + (current_price * SHORT_POSITION['PROFIT_MARGIN']))
-    position['type'] = 'short'
-    order_book_bullish_outer1 = order_book_hit_target(
-            SYMBOLS, 1000, stop_loss, profit_target)
-    print(f'order_book_bullish:{order_book_bullish_outer1}')
-
-
