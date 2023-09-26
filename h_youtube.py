ae7b61f7eb41a7b25f1d936893c498bd620d3c96
will7i7am7 <william.nordberg3@gmail.com>
2023-09-26 11:09:54 +0800

    15sep

--- h_youtube.py
+++ h_youtube.py
@@ -105,7 +105,7 @@ def get_authenticated_service():
                     creds.refresh(Request())
                 except RefreshError:
                     # creds = None
-                    logging.info("Failed to refresh. Need to re-authenticate.")
+                    logging.info("Failed to refresh Youtube token. Need to re-authenticate manually.")
                     return None
 
             # This block is for manually refresh token
